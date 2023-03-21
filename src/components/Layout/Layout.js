065bd3ebf4a7f49fc848173b45e5790ee0be481d
nick.kosmides <kosmides.nick@gmail.com>
2023-03-21 16:13:08 +0200

    analytics

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,21 +2,7 @@ import React from "react";
 import { Menu } from '../Menu';
 import { Footer } from "../Footer";
 import { Link } from "gatsby";
-import { Helmet } from 'react-helmet'
-import GoogleAnalytics from "../GoogleAnalytics/GoogleAnalytics";
 // import {AdSense} from '../AdSense';
 export const Layout = ({ children }) => {
-  return <div className="">
-    <Helmet>
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-NR7DHTPH59"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-NR7DHTPH59');
-</script>
-
-    </Helmet>
-    <Menu/><Link to="/">Home</Link>{children}<Footer/></div>;
+  return <div className=""><Menu/><Link to="/">Home</Link>{children}<Footer/></div>;
 }
