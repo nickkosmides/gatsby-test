99b96d472f0ad32c87578e3876eed99b7d35af96
nick.kosmides <kosmides.nick@gmail.com>
2023-03-25 08:08:37 +0200

    asdasd

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { Menu } from '../Menu';
+import { Helmet } from 'react-helmet';
 import { Footer } from "../Footer";
 import { Link } from "gatsby";
 // import {AdSense} from '../AdSense';
 export const Layout = ({ children }) => {
-  return <div className=""><Menu/>{children}<Footer/></div>;
+  return <div className="">
+      <Helmet>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9889741061727429"
+     crossorigin="anonymous"></script>      </Helmet>
+    <Menu/>{children}<Footer/></div>;
 }
